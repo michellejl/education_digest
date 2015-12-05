40cd6a5e6c732af511a8896b581725822963df49
Brigette Eckert <brigette.eckert@live.com>
2015-12-05 13:36:50 -0800

    pre pull commit

--- js/api/giphy.js
+++ js/api/giphy.js
@@ -1,6 +1,7 @@
 // var term1 = "grace";
 // var term2 = "hopper";
 // var term3= "";
+
 var search = "cat";
 $.ajax({
 	url: 'http://api.giphy.com/v1/stickers/search?q='+search+'&api_key=dc6zaTOxFJmzC',
