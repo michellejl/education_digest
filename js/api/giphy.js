6653d065ea57378f60db8a8e866beb8f286bbf10
Brigette Eckert <brigette.eckert@live.com>
2015-12-05 13:09:19 -0800

    got callback working to access gif associated with  search term

--- js/api/giphy.js
+++ js/api/giphy.js
@@ -1,8 +1,19 @@
+// var term1 = "grace";
+// var term2 = "hopper";
+// var term3= "";
+var search = "cat";
 $.ajax({
-	url: 'http://api.giphy.com/v1/stickers/search?q=cat&api_key=dc6zaTOxFJmzC',
+	url: 'http://api.giphy.com/v1/stickers/search?q='+search+'&api_key=dc6zaTOxFJmzC',
 	type: 'GET',
 	dataType: 'json',
-	data: {images: 'fixed_height'},
+	data: {limit: '1'},
+	success: function(jsonData) {
+		console.log(jsonData);
+		//getting first gif (data is part of array for search term- fixed height)
+		gif = jsonData.data[0].images.fixed_height.url;
+		console.log(gif);
+		$(".image").innerHTML("<img src=" + gif + ">" )
+	}
 })
 
 //change search term to var  - take search term from header
