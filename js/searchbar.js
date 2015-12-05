3090bc4d320815bb577c6fc93d0b5abe5ee78261
luongthomas <luongt@me.com>
2015-12-05 14:02:40 -0800

    Added some javascript functionality to pass the keyword from the searchbar but isn't working

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,9 +1,9 @@
 <!-- JAVASCRIPT to clear search text when the field is clicked -->
 
 $(function() {
-  $("#tfq2b").click(function() {
-    if ($("#tfq2b").val() == "Search our website"){
-      $("#tfq2b").val(""); 
+  $("#searchTerm").click(function() {
+    if ($("#searchTerm").val() == "Search our website"){
+      $("#searchTerm").val(""); 
     }
   });
 });
